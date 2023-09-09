fb5b3c1303cf74569226ee9298582a2ccec74ac7
Alex | Kronox <alexander_vogt@outlook.de>
2023-09-09 09:52:51 +0200

    test1

--- report-viewer/playwright.config.ts
+++ report-viewer/playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -103,7 +103,7 @@ const config: PlaywrightTestConfig = {
      * Use the preview server on CI for more realistic testing.
     Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
+    command: 'vite preview --port 5173',
     port: 5173,
     reuseExistingServer: !process.env.CI
   }
--- report-viewer/tests/e2e/boilerplate.spec.ts
+++ report-viewer/tests/e2e/boilerplate.spec.ts
@@ -1,5 +1,45 @@
-import { test } from '@playwright/test'
+import { expect, test, Page } from '@playwright/test'
 
-test('example', async ({ page }) => {
+test('Test information page', async ({ page }) => {
   await page.goto('/')
+  await uploadFile('result_small_cluster.zip', page)
+
+  // check displayed information on overview page
+  const bodyOverview = await page.locator('body').textContent()
+  expect(bodyOverview).toContain('Directory: files')
+  expect(bodyOverview).toContain('Total Submissions: 4')
+  expect(bodyOverview).toContain('Total Comparisons: 6')
+  expect(bodyOverview).toContain('Min Match Length: 9')
+
+  // go to information page
+  await page.getByText('More').click()
+  await page.waitForURL('/info')
+
+  // check displayed run options on information page
+  const runOptions = await page.getByText('Run Options:Submission Folder:').textContent()
+  expect(runOptions).toContain('Submission Folder: files')
+  expect(runOptions).toContain('Basecode Folder:')
+  expect(runOptions).toContain('Language: Javac based AST plugin')
+  expect(runOptions).toContain('File Extentions: .java, .JAVA')
+  expect(runOptions).toContain('Minimum Token Match:: 9')
+
+  const runData = await page.getByText('Run Data:Date of Execution:').textContent()
+  expect(runData).toContain('Date of Execution: 02/09/23')
+  expect(runData).toContain('Execution Duration: 12 ms')
+  expect(runData).toContain('Submission Count: 4')
+  expect(runData).toContain('Total Comparisons: 6')
+  expect(runData).toContain('Shown Comparisons: 6')
+  expect(runData).toContain('Missing Comparisons: 0')
 })
+
+export async function uploadFile(fileName: string, page: Page) {
+  expect(page).toHaveURL('/')
+
+  // upload file through file chooser
+  const fileChooserPromise = page.waitForEvent('filechooser')
+  await page.getByText('Drag and Drop zip/Json file on this page').click()
+  const fileChooser = await fileChooserPromise
+  await fileChooser.setFiles(`tests/e2e/assets/${fileName}`)
+
+  await page.waitForURL('/overview')
+}
