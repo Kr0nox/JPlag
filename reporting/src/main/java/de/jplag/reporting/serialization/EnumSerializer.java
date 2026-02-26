ae5d78d6cd9cbf73fb64746f969a24cfc961d078
Alexander Milster <alexander.milster@gmx.de>
2026-02-26 10:57:50 +0100

    Simplified Serialization of enums

--- reporting/src/main/java/de/jplag/reporting/serialization/EnumSerializer.java
+++ reporting/src/main/java/de/jplag/reporting/serialization/EnumSerializer.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 /**
- * Serializes enums only exporting the name of the enum constant
+ * Serializes enums only exporting the name of the enum constant.
  */
 public class EnumSerializer extends StdSerializer<Enum> {
     protected EnumSerializer() {
