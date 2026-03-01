cf98914daa871046136003f068d856448e631bf5
Timur Saglam <4396919+tsaglam@users.noreply.github.com>
2026-03-01 16:43:53 +0100

    Remove metamodel parsing from the dynamic model parser. If a submission contains metamodel files, a warning will be displayed.

--- languages/emf-model/src/main/java/de/jplag/emf/model/parser/DynamicModelParser.java
+++ languages/emf-model/src/main/java/de/jplag/emf/model/parser/DynamicModelParser.java
@@ -1,11 +1,7 @@
 package de.jplag.emf.model.parser;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,11 +21,7 @@ public class DynamicModelParser extends DynamicEcoreParser {
     private static final Logger logger = LoggerFactory.getLogger(DynamicModelParser.class);
 
     private static final String VIEW_FILE_WARNING = "Skipping view file {} as submission!";
-    private static final String METAPACKAGE_WARNING = "Loading model instance {} without any metamodel!";
-    private static final String METAPACKAGE_ERROR = "Error, not a metapackage: ";
-    private static final String METAMODEL_LOADING_ERROR = "Could not load metamodel file!";
-
-    private static final List<EPackage> metapackages = new ArrayList<>();
+    private static final String METAMODEL_WARNING = "Metamodel files like {} should be specified via the language options!";
     private static final String ALL_EXTENSIONS = "*";
 
     /**
@@ -43,13 +35,10 @@ public class DynamicModelParser extends DynamicEcoreParser {
     protected void parseModelFile(File file, boolean normalize) throws ParsingException {
         // implicit assumption: Metamodel gets parsed first!
         if (file.getName().endsWith(EmfLanguage.FILE_ENDING)) {
-            parseMetamodelFile(file);
+            logger.warn(METAMODEL_WARNING, file.getName());
         } else if (file.getName().endsWith(EmfLanguage.VIEW_FILE_EXTENSION)) {
             logger.warn(VIEW_FILE_WARNING, file.getName());
         } else {
-            if (metapackages.isEmpty()) {
-                logger.warn(METAPACKAGE_WARNING, file.getName());
-            }
             super.parseModelFile(file, normalize);
         }
     }
@@ -62,21 +51,5 @@ public class DynamicModelParser extends DynamicEcoreParser {
     @Override
     protected AbstractModelView createView(File file, Resource modelResource) {
         return new GenericEmfTreeView(file, modelResource);
-    }
-
-    private void parseMetamodelFile(File file) throws ParsingException {
-        metapackages.clear();
-        Resource modelResource = EMFUtil.loadModelResource(file);
-        if (modelResource == null) {
-            throw new ParsingException(file, METAMODEL_LOADING_ERROR);
-        }
-        for (EObject object : modelResource.getContents()) {
-            if (object instanceof EPackage ePackage) {
-                metapackages.add(ePackage);
-            } else {
-                logger.error(METAPACKAGE_ERROR, object);
-            }
-        }
-        EMFUtil.registerEPackageURIs(metapackages);
     }
 }
